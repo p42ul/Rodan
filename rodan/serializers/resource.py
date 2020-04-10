599442639ad1500b098d82c582f414e4fe6a2cbc
deepio <alex.daigle2@gmail.com>
2020-04-09 23:15:43 -0400

    Update

--- rodan/serializers/resource.py
+++ rodan/serializers/resource.py
@@ -7,6 +7,11 @@ from rodan.serializers import AbsoluteURLField
 class ResourceSerializer(serializers.HyperlinkedModelSerializer):
     uuid = serializers.CharField(read_only=True)
     creator = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    label = serializers.SlugRelatedField(
+        slug_field="resource_label",
+        many=True,
+        allow_null=True
+    )
     resource_file = AbsoluteURLField(source="resource_url", read_only=True)
     # compat_resource_file = AbsoluteURLField(source='compat_file_url', read_only=True)
     small_thumb = AbsoluteURLField(source="small_thumb_url", read_only=True)
