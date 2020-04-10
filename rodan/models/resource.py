fd588d53027f5a28f7d3f7f7f8e2d87ab2f27ce4
deepio <alex.daigle2@gmail.com>
2020-04-09 23:10:02 -0400

    Modified: Adjust resourcelabel

--- rodan/models/resource.py
+++ rodan/models/resource.py
@@ -9,6 +9,7 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 from rodan.constants import task_status
+from rodan.models import ResourceLabel
 
 logger = logging.getLogger("rodan")
 
@@ -141,7 +142,7 @@ class Resource(models.Model):
 
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     updated = models.DateTimeField(auto_now=True, db_index=True)
-    label = models.ManyToManyField("Label")
+    label = models.ManyToManyField("ResourceLabel")
 
     def save(self, *args, **kwargs):
         super(Resource, self).save(*args, **kwargs)
@@ -294,33 +295,3 @@ class Resource(models.Model):
     def viewer_relurl(self):
         if self.get_viewer() is not None:
             return reverse("resource-viewer-acquire", args=(self.uuid,))
-
-
-class Label(models.Model):
-    """
-    A `Label` is associated with a Rodan Resource.
-
-    **Fields**
-
-    - `name` -- user-assigned name of this `Label`.
-    - `uuid` -- unique identifier as a primary key.
-    - `creator` -- a reference to the `User`.
-    """
-    name = models.CharField(max_length=30)
-    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    # creator = models.ForeignKey(
-    #     User,
-    #     related_name="label",
-    #     null=True,
-    #     blank=True,
-    #     on_delete=models.SET_NULL,
-    #     db_index=True,
-    # )
-
-    class Meta:
-        app_label = "rodan"
-        permissions = (("view_resource", "View Resource"),)
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
